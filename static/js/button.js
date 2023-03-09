fd563115785b4de8651652c06e04be3e1a517cd3
Eyner-schoonewolff <enewolff2013@gmail.com>
2023-03-08 23:14:19 -0500

    tarea registro finalizado

--- static/js/button.js
+++ static/js/button.js
@@ -1,9 +1,11 @@
 document.querySelector('#formulario').addEventListener('keyup', () => {
-    const usuario =document.querySelector('#usuario')
-    const contrasenia =document.querySelector('#contrasenia')
+    const nombre = document.querySelector('#nombre')
+    const email = document.querySelector('#email')
+    const contrasenia = document.querySelector('#contrasenia')
+    const documento = document.querySelector('#numeroDocumento')
     const btn = document.querySelector('#btn-registro')
-    let desativar=false
-    if (usuario.value == '' || contrasenia.value == '') {
+    let desativar = false
+    if (nombre.value == '' || contrasenia.value == '' || email.value == '' || documento.value == '') {
         desativar = true
     }
     if (desativar == true) {
