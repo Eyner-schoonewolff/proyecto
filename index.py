7eef9c1a2bc168687b61e1780006c98a6ceb019e
Eyner-schoonewolff <enewolff2013@gmail.com>
2023-03-18 00:01:27 -0500

    cambios proyecto avances

--- index.py
+++ index.py
@@ -3,12 +3,14 @@ from router.router import login,login_manager
 from router.registrar import registrar
 from router.datos_personales import datos_personales
 from router.menu import menus
+from router.Solicitar_servicios import solicitar_servi
 
 app = Flask(__name__)
 app.register_blueprint(login)
 app.register_blueprint(registrar)
 app.register_blueprint(datos_personales)
 app.register_blueprint(menus)
+app.register_blueprint(solicitar_servi)
 login_manager.init_app(app)
 app.config['SECRET_KEY'] = 'secret-key-goes-here'
 
--- static/js/fecha_solicitud.js
+++ static/js/fecha_solicitud.js
@@ -1,8 +1,50 @@
-// Obtener el elemento de fecha
-var fecha = document.getElementById("fecha");
 
-// Obtener la fecha actual
-var hoy = new Date().toISOString().split("T")[0];
+var fecha_act = new Date();
+$('#fecha').datetimepicker({
+    format: 'YYYY-MM-DD',
+    minDate: fecha_act,
 
-// Establecer el valor mínimo como la fecha actual
-fecha.setAttribute("min", hoy);
+})
+
+
+function guardarsolicitud() {
+    let fecha = $("#fecha").val();
+    let hora = $("#hora").val();
+    let servicio = $("#opciones").val();
+    let contratista = $("#contratistas").val();
+    let evidencia = $("#formFileSm").val();
+    let problema = $("#carta").val();
+
+    datos = {
+        fecha,
+        hora,
+        servicio,
+        contratista,
+        evidencia,
+        problema
+
+    }
+
+    $.ajax({
+        url: '/solicitar_serv',
+        method: 'POST',
+        data: JSON.stringify(datos),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+
+        success: function (respuesta) {
+            console.log(respuesta.numero);
+            if (respuesta.numero == 1) {
+                alert('Solicitud Creada')
+                $("#fecha").val('');
+                $("#hora").val('');
+                $("#opciones").val();
+                $("#contratistas").val();
+                $("#formFileSm").val();
+                $("#carta").val('');
+            } else {
+                alert('Error')
+            }
+        }
+    });
+}
