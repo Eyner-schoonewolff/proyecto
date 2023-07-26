85327859df8c1399b3aef9a1bdc3172a458b43da
Eyner-schoonewolff <enewolff2013@gmail.com>
2023-07-26 00:48:29 -0500

    ...

--- controlador/c_solicitar_servicios.py
+++ controlador/c_solicitar_servicios.py
@@ -1,17 +1,28 @@
 from flask import request, redirect, url_for
 import os
 import uuid
-from werkzeug.utils import secure_filename
 from seguridad.Model_solicitar_servicio import Solicitar
 from decorador.decoradores import *
 from flask_jwt_extended import get_jwt_identity
 from PIL import Image
 import base64
-import imghdr
-import io
+from io import BytesIO
+import asyncio
 
 
 class Solicitar_controlador():
+
+    async def guardar_imagen(self, file):
+        image = self.base64_to_image(file)
+        extension = self.get_base64_extension(image)
+        nuevo_nombre_file = str(uuid.uuid4()) + '.' + extension
+        upload_path = os.path.join('static/img', nuevo_nombre_file)
+
+        # Guarda los datos binarios en el archivo en el sistema de archivos.
+        image.save(upload_path)
+
+        return nuevo_nombre_file
+
     def servicio(self):
         identificadores = get_jwt_identity()
         id_usuario = identificadores.get('id')
@@ -27,27 +38,26 @@ class Solicitar_controlador():
             tipo_contratista = json['servicio']
             file = json['evidencia']
 
-            print(file)
             if file:
-                image = self.base64_to_image(file)
-                extension = self.get_base64_extension(file)
-                nuevo_nombre_file = str(uuid.uuid4()) + extension
-                upload_path = os.path.join('static/img', nuevo_nombre_file)
-                # Guarda los datos binarios en el archivo en el sistema de archivos.
-                with open(upload_path, 'wb') as f:
-                    f.write(image)
+                asyncio.run(self.guardar_imagen(file))
+                # asyncio.create_task(self.guardar_imagen(file))
+                # image = self.base64_to_image(file)
+                # extension = self.get_base64_extension(image)
+                # nuevo_nombre_file = str(uuid.uuid4()) +'.'+ extension
+                # upload_path = os.path.join('static/img', nuevo_nombre_file)
+                # # Guarda los datos binarios en el archivo en el sistema de archivos.
+                # image.save(upload_path)
             else:
                 nuevo_nombre_file = ""
 
-            print(nuevo_nombre_file)
-            solicitar = Solicitar(fecha=fecha,
-                                  hora=hora,
-                                  contratista=contratista,
-                                  tipo_contratista=tipo_contratista,
-                                  evidencia=nuevo_nombre_file,
-                                  problema=problema,
-                                  id_usuario=id_usuario
-                                  )
+            # solicitar = Solicitar(fecha=fecha,
+            #                       hora=hora,
+            #                       contratista=contratista,
+            #                       tipo_contratista=tipo_contratista,
+            #                       evidencia=nuevo_nombre_file,
+            #                       problema=problema,
+            #                       id_usuario=id_usuario
+            #                       )
             # valor = solicitar.agregar()
             valor = True
 
@@ -58,28 +68,22 @@ class Solicitar_controlador():
         else:
             return {'nombre': nombre, 'tipo': tipo}
 
-    def cancelar(self, id):
-        identificadores = get_jwt_identity()
-        tipo_usuario = identificadores.get('tipo_usuario')
-        eliminar_solicitud = Solicitar()
+    def base64_to_image(self, base64_string: str):
+        bytes_image = base64.b64decode(base64_string.split(",")[1])
+        image = Image.open(BytesIO(bytes_image))
+        return image
 
-        if eliminar_solicitud.eliminar(id=id) and tipo_usuario != 'Admin':
-            return redirect(url_for('menus.consultar'))
-        else:
-            eliminar_solicitud.eliminar(id=id)
-            return redirect(url_for('menus.consultar_admin'))
+    # def cancelar(self, id):
+    #     identificadores = get_jwt_identity()
+    #     tipo_usuario = identificadores.get('tipo_usuario')
+    #     eliminar_solicitud = Solicitar()
 
-    def base64_to_image(self, base64_string):
-        # Elimina la parte inicial del string de Base64 para obtener solo los datos binarios.
-        encoded = base64_string.split(',', 1)
-        # Decodifica el string Base64 a datos binarios.
-        decoded_data = base64.b64decode(encoded)
-        return decoded_data
+    #     if eliminar_solicitud.eliminar(id=id) and tipo_usuario != 'Admin':
+    #         return redirect(url_for('menus.consultar'))
+    #     else:
+    #         eliminar_solicitud.eliminar(id=id)
+    #         return redirect(url_for('menus.consultar_admin'))
 
-    def get_base64_extension(self, base64_string):
-        # Analiza el string Base64 para extraer la extensión del archivo.
-        metadata = base64_string.split(',', 1)
-        if 'image/jpeg' in metadata:
-            return '.jpg'
-        elif 'image/png' in metadata:
-            return '.png'
+    def get_base64_extension(self, img: str):
+        img_format = img.format.lower()
+        return img_format
