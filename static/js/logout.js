df7185597fdbdd33b2c3e8a944db9bb66dd77e37
Eyner-schoonewolff <enewolff2013@gmail.com>
2023-10-01 22:30:56 -0500

    reajustes de socketweb

--- static/js/logout.js
+++ static/js/logout.js
@@ -1,25 +1,30 @@
 var token = localStorage.getItem('jwt-token');
+var id = localStorage.getItem('id');
 
-function logout(){
+function logout() {
   document.querySelector("#logout")
-  .addEventListener("click",
-    () => {
-      $.ajax({
+    .addEventListener("click",
+      () => {
+        $.ajax({
           url: 'http://localhost:3000/logout',
           method: 'GET',
           dataType: 'json',
           contentType: 'application/json',
           headers: {
-              'Content-Type': 'application/json',
-              'Authorization': 'Bearer ' + token
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
           },
           success: function (respuesta) {
-              if(respuesta.message=='ok'){
-                localStorage.removeItem('jwt-token');
-                window.location.href='../templates/index.html'
-              }
+            if (respuesta.message == 'ok') {
+              cerrar_sala(id);
+              localStorage.removeItem('jwt-token');
+              localStorage.removeItem('id');
+              localStorage.removeItem('exp');
+              localStorage.removeItem('exp_seg');
+              window.location.href = '../templates/index.html'
+            }
           }
-      });
-    }
-  );
+        });
+      }
+    );
 }
--- static/js/socket_notificacion.js
+++ static/js/socket_notificacion.js
@@ -3,22 +3,33 @@
 var socket = io('http://localhost:3000')
 
 function enviar_notificacion(data) {
+    // informacion emite del froned al servidor
     socket.emit("mi_evento", data);
 }
 
+function cerrar_sala(data) {
+    socket.emit("leave", data);
+}
 
 socket.on('connect', function () {
     console.log('Conectado al servidor');
 
+    // Primero se une a la sala cuando se loguea
     socket.on('join', function (data) {
         console.log('Join sala', data);
 
+        // luego dentro de la sala escucha la notificacion y le mando la informacion emitida
+        socket.on('mi_evento', function (info) {
+            // informacion que recibe del backend
+            console.log('notificacion', info);
+
+        });
+        socket.on('leave', function (id) {
+            console.log('sesion cerrada', id);
+
+        });
     });
 
-    socket.on('mi_evento', function (data) {
-        console.log('notificacion', data);
-
-    });
 });
 
 
